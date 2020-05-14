d96844188bc8c8f62dcfd04cdfdf6fa558468d05
Joseph765 <joseph.chauvin102@gmail.com>
2020-05-14 18:38:30 -0500

    changed proxy middleware to match deployed versions

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const port = 2000;
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use('/:id', express.static(path.join(__dirname, '/../public')))
 
-app.use('/products/:id', createProxyMiddleware({target: 'http://localhost:4000', changeOrigin: true}));
-app.use('/get/random', createProxyMiddleware({target: 'http://localhost:4000', changeOrigin: true}));
-app.use('/product/:id', createProxyMiddleware({target: 'http://localhost:4200', changeOrigin: true}));
-app.use('/reviews/:id', createProxyMiddleware({target: 'http://localhost:5000', changeOrigin: true}));
-app.use('/api/carousel/:id', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}));
-app.use('/api/carouselEnlarged/:id', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}));
+app.use('/products/:id', createProxyMiddleware({target: 'http://ec2-3-22-170-203.us-east-2.compute.amazonaws.com:4000', changeOrigin: true}));
+app.use('/get/random', createProxyMiddleware({target: 'http://ec2-3-22-170-203.us-east-2.compute.amazonaws.com:4000', changeOrigin: true}));
+app.use('/product/:id', createProxyMiddleware({target: 'http://18.217.185.35:4200', changeOrigin: true}));
+// app.use('/reviews/:id', createProxyMiddleware({target: 'http://localhost:5000', changeOrigin: true}));
+// app.use('/api/carousel/:id', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}));
+// app.use('/api/carouselEnlarged/:id', createProxyMiddleware({target: 'http://localhost:9000', changeOrigin: true}));
 
 app.listen(port, (err) => {
   console.log(`Server is running on port ${port}!`);
